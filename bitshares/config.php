1c3b83db76ca8f5b288c50432ba982e365780486
sidhujag <sidhujag@hotmail.com>
2015-01-19 22:25:13 -0800

    initial commit

---
+++ bitshares/config.php
@@ -0,0 +1,11 @@
+<?php
+define('ROOT', __DIR__ .DIRECTORY_SEPARATOR);
+$baseURL = 'http://www.bitsharesdemo.com/drupal6/';
+$accountName = 'opencartdemo';
+$rpcUser = 'user';
+$rpcPass = 'pass';
+$rpcPort = 1234;
+$demoMode = TRUE;
+$hashSalt = 'drupal6demo'; // see README
+$cronToken = 'asddvsdf43'; // see README
+?>
